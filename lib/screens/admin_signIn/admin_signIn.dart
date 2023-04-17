65e531f28fa30da686002f784fd85035c3854cbb
Ritesh Sinha <ritesh.sinha.737001@gmail.com>
2023-04-17 11:32:51 +0530

    all are get responsive

--- lib/screens/admin_signIn/admin_signIn.dart
+++ lib/screens/admin_signIn/admin_signIn.dart
@@ -19,6 +19,7 @@ class AdminSignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: whiteColor,
       body: SafeArea(
         top: true,
--- lib/screens/place_your_bid/place_your_bid.dart
+++ lib/screens/place_your_bid/place_your_bid.dart
@@ -27,79 +27,80 @@ class PlaceYourBid extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(34, 37, 34, 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                ImagePath.house,
-                height: 185,
-                width: MediaQuery.of(context).size.width,
-              ),
-              const SizedBox(height: 37),
-              Row(
-                children: [
-                  BoldText("Property Title", deepGreer, 15),
-                  const SizedBox(width: 10),
-                  BoldText("PKR  2CR", deepBlue, 13),
-                ],
-              ),
-              const SizedBox(height: 8),
-              LightText("by Asif Raza|asif@gmail.com", deepGreer, 12),
-              const SizedBox(height: 8),
-              ParagraphText(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in scelerisque velit, a interdum libero. In quis molestie nunc. Pellentesque lacinia pulvinar felis non faucibus. Suspendisse potenti.",
-                  deepGreer,
-                  15,
-                  TextAlign.left),
-              const SizedBox(height: 45),
-              TextFormField(
-                autofocus: false,
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(
-                  fillColor: whiteColor,
-                  filled: true,
-                  contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                  hintText: "enter your amount",
-                  hintStyle: const TextStyle(
-                      fontFamily: "Poppins", color: hint, fontSize: 15),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: const BorderSide(color: deepBlue)),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  ImagePath.house,
+                  height: 185,
+                  width: MediaQuery.of(context).size.width,
                 ),
-              ),
-              const SizedBox(height: 25),
-              TextFormField(
-                autofocus: false,
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(
-                  fillColor: whiteColor,
-                  filled: true,
-                  contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                  hintText: "any message/offer",
-                  hintStyle: const TextStyle(
-                      fontFamily: "Poppins", color: hint, fontSize: 15),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: const BorderSide(color: deepBlue)),
+                const SizedBox(height: 37),
+                Row(
+                  children: [
+                    BoldText("Property Title", deepGreer, 15),
+                    const SizedBox(width: 10),
+                    BoldText("PKR  2CR", deepBlue, 13),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 80),
-              MaterialButton(
-                color: deepBlue,
-                height: 40,
-                minWidth: MediaQuery.of(context).size.width,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(28)),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const BuyProperty()));
-                },
-                child: BoldText("Place Bid", whiteColor, 18),
-              ),
-              const Spacer()
-            ],
+                const SizedBox(height: 8),
+                LightText("by Asif Raza|asif@gmail.com", deepGreer, 12),
+                const SizedBox(height: 8),
+                ParagraphText(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in scelerisque velit, a interdum libero. In quis molestie nunc. Pellentesque lacinia pulvinar felis non faucibus. Suspendisse potenti.",
+                    deepGreer,
+                    15,
+                    TextAlign.left),
+                const SizedBox(height: 45),
+                TextFormField(
+                  autofocus: false,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    fillColor: whiteColor,
+                    filled: true,
+                    contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    hintText: "enter your amount",
+                    hintStyle: const TextStyle(
+                        fontFamily: "Poppins", color: hint, fontSize: 15),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: deepBlue)),
+                  ),
+                ),
+                const SizedBox(height: 25),
+                TextFormField(
+                  autofocus: false,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    fillColor: whiteColor,
+                    filled: true,
+                    contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                    hintText: "any message/offer",
+                    hintStyle: const TextStyle(
+                        fontFamily: "Poppins", color: hint, fontSize: 15),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: const BorderSide(color: deepBlue)),
+                  ),
+                ),
+                const SizedBox(height: 70),
+                MaterialButton(
+                  color: deepBlue,
+                  height: 40,
+                  minWidth: MediaQuery.of(context).size.width,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(28)),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const BuyProperty()));
+                  },
+                  child: BoldText("Place Bid", whiteColor, 18),
+                ),
+              ],
+            ),
           ),
         ),
       ),
