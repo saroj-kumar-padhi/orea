8a4c70863982b6bb54f8f12a3ba3b239431f8c7d
Ritesh Sinha <ritesh.sinha.737001@gmail.com>
2023-05-09 01:45:51 +0530

    all commit done

--- lib/screens/place_your_bid/place_your_bid.dart
+++ lib/screens/place_your_bid/place_your_bid.dart
@@ -43,10 +43,10 @@ class _PlaceYourBidState extends State<PlaceYourBid> {
 
     final FirebaseFirestore firestore = FirebaseFirestore.instance;
     final CollectionReference propertiesRef = firestore.collection('users');
-    final TextEditingController amountContrller = TextEditingController();
-    final TextEditingController amountDescription = TextEditingController();
+    final TextEditingController amount = TextEditingController();
+    final TextEditingController description = TextEditingController();
+    
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: whiteColor,
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -86,7 +86,7 @@ class _PlaceYourBidState extends State<PlaceYourBid> {
                   children: [
                     BoldText(widget.propertyTitle, deepGreer, 15),
                     const SizedBox(width: 10),
-                    BoldText("PKR  ${widget.amount}", deepBlue, 13),
+                    BoldText("PKR ${widget.amount}", deepBlue, 13),
                   ],
                 ),
                 const SizedBox(height: 8),
@@ -95,66 +95,89 @@ class _PlaceYourBidState extends State<PlaceYourBid> {
                 ParagraphText(
                     widget.discription, deepGreer, 15, TextAlign.left),
                 const SizedBox(height: 45),
-                TextFormField(
-                  controller: amountContrller,
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                    fillColor: whiteColor,
-                    filled: true,
-                    contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                    hintText: "enter your amount",
-                    hintStyle: const TextStyle(
-                        fontFamily: "Poppins", color: hint, fontSize: 15),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: const BorderSide(color: deepBlue)),
+                Container(
+                  height: 150,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: whiteColor,
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ),
-                const SizedBox(height: 25),
-                TextFormField(
-                  controller: amountDescription,
-                  autofocus: false,
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                    fillColor: whiteColor,
-                    filled: true,
-                    contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                    hintText: "any message/offer",
-                    hintStyle: const TextStyle(
-                        fontFamily: "Poppins", color: hint, fontSize: 15),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: const BorderSide(color: deepBlue)),
-                  ),
-                ),
-                const SizedBox(height: 80),
-                MaterialButton(
-                  color: deepBlue,
-                  height: 40,
-                  minWidth: MediaQuery.of(context).size.width,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(28)),
-                  onPressed: () async {
-                    // get the document reference for the property
-                    final DocumentReference propertyRef =
-                        propertiesRef.doc(widget.id);
-
-                    // update the status of the property to approved
-                    await propertyRef.update({
-                      'bidAmount': amountContrller.text,
-                      'bidDescription': amountDescription.text
-                    });
-
-                    // rebuild the list view to reflect the updated status
-                    setState(() {});
-
-                    // ignore: use_build_context_synchronously
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RealEstateBidding()));
-                  },
-                  child: BoldText("Place Bid", whiteColor, 18),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(
+                          controller: amount,
+                          autofocus: true,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            fillColor: whiteColor,
+                            filled: true,
+                            contentPadding:
+                                const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                            hintText: "enter your amount",
+                            hintStyle: const TextStyle(
+                                fontFamily: "Poppins",
+                                color: hint,
+                                fontSize: 15),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: const BorderSide(color: deepBlue)),
+                          ),
+                          onEditingComplete: () =>
+                              FocusScope.of(context).nextFocus(),
+                        ),
+                        const SizedBox(height: 25),
+                        // TextFormField(
+                        //   controller: description,
+                        //   keyboardType: TextInputType.name,
+                        //   decoration: InputDecoration(
+                        //     fillColor: whiteColor,
+                        //     filled: true,
+                        //     contentPadding:
+                        //         const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                        //     hintText: "any message/offer",
+                        //     hintStyle: const TextStyle(
+                        //         fontFamily: "Poppins",
+                        //         color: hint,
+                        //         fontSize: 15),
+                        //     border: OutlineInputBorder(
+                        //         borderRadius: BorderRadius.circular(30),
+                        //         borderSide: const BorderSide(color: deepBlue)),
+                        //   ),
+                        //   onEditingComplete: () =>
+                        //       FocusScope.of(context).unfocus(),
+                        // ),
+                        const SizedBox(height: 20),
+                        MaterialButton(
+                          color: deepBlue,
+                          height: 40,
+                          minWidth: MediaQuery.of(context).size.width,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(28)),
+                          onPressed: () async {
+                            // get the document reference for the property
+                            final DocumentReference propertyRef =
+                                propertiesRef.doc(widget.id);
+          
+                            // update the status of the property to approved
+                            await propertyRef.update({
+                              'bidAmount': amount.text,
+                              'bidDescription': description.text
+                            });
+          
+                            // rebuild the list view to reflect the updated status
+                            setState(() {});
+          
+                            // ignore: use_build_context_synchronously
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const RealEstateBidding()));
+                          },
+                          child: BoldText("Place Bid", whiteColor, 18),
+                        ),
+                      ]),
                 ),
               ],
             ),
